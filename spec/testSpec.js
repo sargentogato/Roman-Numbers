2b4110bfd68623951819742a92904e64eb1d7c8d
Marlon Velasquez <marlonjvelasquez@gmail.com>
2020-03-19 10:53:46 +0100

    RED -The test is in red -Added a second test

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,9 +1,17 @@
 describe("Roman number converter", () => {
   it("Convert 1 to roman number", () => {
-    const romanNumber = "I";
+    const romanNumber = 1;
 
     const result = add(romanNumber);
 
     expect(result).toEqual("I");
+  });
+
+  it("Convert 5 to roman number", () => {
+    const romanNumber = 1;
+
+    const result = add(romanNumber);
+
+    expect(result).toEqual("V");
   });
 });
